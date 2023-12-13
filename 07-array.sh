0e60d4e76a6a1368a1ce49b4598d3e3b0c171a96
knbob <email2nagababu@gmail.com>
2023-12-13 06:23:07 +0530

    shell scrip

--- 07-array.sh
+++ 07-array.sh
@@ -5,3 +5,4 @@ FRUITS=("APPLE" "BANANA" "MANGO")
 echo " First value : $FRUITS[0]"
 echo " SECOND value : $FRUITS[0]"
 echo " THIRD value : $FRUITS[0]"
+echo " all fruits: $FRUITS[@]"    #@ WILL GET ALL THE VALUES IN ARRAY
