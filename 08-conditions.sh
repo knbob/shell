d7a3aec28029c0e2b9f7decdf85d1d7c1c5a2f80
knbob <email2nagababu@gmail.com>
2023-12-13 18:08:02 +0530

    shell

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -5,4 +5,9 @@ NUMBER1=$1
 if [ $NUMBER1 -gt 100 ]
 then
 echo "Given number $NUMBER1 is greater than 100"
+if [ $NUMBER1 == 100 ]
+then
+echo "Given number $NUMBER1 is equal to 100"
+else
+echo "Given number $NUMBER1 is less than 100"
 fi
