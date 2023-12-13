71bfb311a787796b0eb7e01e5987072d9c228b3f
knbob <email2nagababu@gmail.com>
2023-12-13 18:28:15 +0530

    shell

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 NUMBER1=$1
-NUMBER2=3
+NUMBER2=2
 
 DIV=$(($NUMBER1 / $NUMBER2))
 REM=$(($NUMBER1 % $NUMBER2))
 echo "Quatient is $DIV"
 echo "Remainder is $REM"
+
+if [ $REM == 0 ]
+then
+echo "Given number $NUMBER1 is even number"
+else
+echo "Given number $NUMBER1 is odd number"
