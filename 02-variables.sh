677f2658431cd5b330ccea5e66e026c162a973dd
knbob <email2nagababu@gmail.com>
2023-12-13 05:47:12 +0530

    shell scrip

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-echo "Ramesh : Hello Suresh, Good morning!!"
-echo "Suresh: Hi Ramesh, very good morning !!"
-echo "Ramesh : How are you Suresh"
-echo "Suresh : I am good, how are you Ramesh"
+# With out variables, names are repeated 
+
+# echo "Ramesh : Hello Suresh, Good morning!!"
+# echo "Suresh: Hi Ramesh, very good morning !!"
+# echo "Ramesh : How are you Suresh"
+# echo "Suresh : I am good, how are you Ramesh"
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1 : Hello $PERSON2, Good morning!!"
+echo "$PERSON2: Hi $PERSON1, very good morning !!"
+echo "$PERSON1 : How are you $PERSON2"
+echo "$PERSON2 : I am good, how are you $PERSON1"
