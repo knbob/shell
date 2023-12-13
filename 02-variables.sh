e42f29c20a33869598a0e269be086f73431d14a5
knbob <email2nagababu@gmail.com>
2023-12-13 05:51:23 +0530

    shell scrip

--- 02-variables.sh
+++ 02-variables.sh
@@ -7,8 +7,8 @@
 # echo "Ramesh : How are you Suresh"
 # echo "Suresh : I am good, how are you Ramesh"
 
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=Naga          #there shouldn't be no space between variable name and value.
+PERSON2=Babu
 
 echo "$PERSON1 : Hello $PERSON2, Good morning!!"
 echo "$PERSON2: Hi $PERSON1, very good morning !!"
