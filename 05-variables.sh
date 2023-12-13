ffe230550721f3a3c895dd9648ca90de729524e7
knbob <email2nagababu@gmail.com>
2023-12-13 06:12:12 +0530

    shell scrip

--- 05-variables.sh
+++ 05-variables.sh
@@ -21,7 +21,8 @@
 # Above programme exposes the values to the external. To hide the values we need to use read -s command.
 
 echo "enter your user name: "
-
 read -s USERNAME    # this command will take input from user and stores in variable --> read <variable name>
+
+echo "enter your password: " # this line will print first and then asks the input from user while running.
 read -s PASSWORD
 echo "username is : $USERNAME and password is : $PASSWORD "
