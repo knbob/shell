e724d6b53e3c8931d6dafbc27e7df7838a662e92
knbob <email2nagababu@gmail.com>
2023-12-13 06:07:49 +0530

    shell scrip

---
+++ 05-variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# THIS programme will take user credentials from terminal but its exposed to external
+
+# USERNAME=$1
+# PASSWORD=$2
+# echo "username is : $USERNAME and password is : $PASSWORD "
+
+
+# This programe will take inputs from user while running the programme.
+
+echo "enter your user name: "
+
+read USERNAME    # this command will take input from user and stores in variable --> read <variable name>
+
+echo "username is : $USERNAME and password is : PASSWORD "
