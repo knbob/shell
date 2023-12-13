d40e92c1e8105f20c6824795f04d6b0388f1976c
knbob <email2nagababu@gmail.com>
2023-12-13 06:00:33 +0530

    shell scrip

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# this programming taking arguments/args from command line.
+
+PERSON1=$1          #there shouldn't be no space between variable name and value.
+PERSON2=$2
+
+echo "$PERSON1 : Hello $PERSON2, Good morning!!"
+echo "$PERSON2: Hi $PERSON1, very good morning !!"
+echo "$PERSON1 : How are you $PERSON2"
+echo "$PERSON2 : I am good, how are you $PERSON1"
