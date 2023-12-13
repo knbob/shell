3287dcdeaaeafcbcafba4b20325eecd021ec66d9
knbob <email2nagababu@gmail.com>
2023-12-13 05:40:26 +0530

    shell scrip

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh : Hello Suresh, Good morning!!"
+echo "Suresh: Hi Ramesh, very good morning !!"
+echo "Ramesh : How are you Suresh"
+echo "Suresh : I am good, how are you Ramesh"
