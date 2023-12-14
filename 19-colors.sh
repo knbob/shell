7b6f15b5fa4fd6f7fdf62cdf5c915117f72178c3
knbob <email2nagababu@gmail.com>
2023-12-14 16:31:42 +0530

    Shell script

--- 19-colors.sh
+++ 19-colors.sh
@@ -3,8 +3,9 @@ g="\e[32m"
 y="\e[33m
 N="\e[0m"
 
+echo -e "Hello Iam learning \e[31m Shell script"
 
-echo -e "\e[31m Hello $g how are you $y dear"
+echo -e " \e[31m Hello $g how are you $y dear"
 
 echo -e " $r printing in red - I am good bro"
 
