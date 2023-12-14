45ccb4a90a0f0c89432619717c9b469c94f3135e
knbob <email2nagababu@gmail.com>
2023-12-14 16:02:50 +0530

    Shell script

--- 18-logs.sh
+++ 18-logs.sh
@@ -8,26 +8,25 @@ ID=$(id -u)
 TIMESTAMP=$(date +%F:%H:%M:%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-yum install mysql -y &>> $LOGFILE
+VALIDATE(){
+    if [ $1 == 0 ]
+    then
+    echo " $2 is success"
+    else
+    echo "$2 is failed"
+    fi
+}
 
-# VALIDATE(){
-#     if [ $1 == 0 ]
-#     then
-#     echo " $2 is success"
-#     else
-#     echo "$2 is failed"
-#     fi
-# }
 
-# if [ $ID == 0 ]
-# then
-# echo "user logged in as root user"
-# else
-# echo " Please loggin as root user"
-# exit 1
-# fi
+if [ $ID == 0 ]
+then
+echo "user logged in as root user"
+else
+echo " Please loggin as root user"
+exit 1
+fi
 
-# yum install git -y &>> $LOGFILE
-# VALIDATE $? "Installing git package"
-# yum install nginx -y &>> $LOGFILE
-# VALIDATE $? "Installing nginx package"
+yum install git -y &>> $LOGFILE
+VALIDATE $? "Installing git package"
+yum install nginx -y &>> $LOGFILE
+VALIDATE $? "Installing nginx package"
