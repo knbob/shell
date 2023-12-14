8d779ff7e5fec9c26fbf18d0b4ceca3585a8c214
knbob <email2nagababu@gmail.com>
2023-12-14 11:05:48 +0530

    Shell script

---
+++ 17-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+ID=$(id -u)
+
+VALIDATE(){
+    if [ $1 == 0 ]
+    then
+    echo " $2 is success"
+    else
+    echo "$2 is failed"
+    fi
+}
+
+if [ $ID == 0 ]
+then
+echo "user logged in as root user"
+else
+echo " Please loggin as root user"
+fi
+
+yum install git -y
+VALIDATE $? "Installing git package"
+yum install nginx -y
+VALIDATE $? "Installing nginx package"
