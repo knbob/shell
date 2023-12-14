71e1b86b108dcabe540eac46c81ba2e3d8b1ffa2
knbob <email2nagababu@gmail.com>
2023-12-14 16:26:34 +0530

    Shell script

---
+++ 19-colors.sh
@@ -0,0 +1,13 @@
+r="\e[31m"
+g="\e[32m"
+y="\e[33m
+N="\e[0m"
+
+
+echo -e "$r Hello $g how are you $y dear"
+
+echo -e "$r printing in red - I am good bro"
+
+echo -e "$y prinitng in yellow - good morning bro"
+
+echo -e "$N its normalized
