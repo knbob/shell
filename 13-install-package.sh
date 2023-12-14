a320f87ee24bee5082e36758d41ef0805dbdbf51
knbob <email2nagababu@gmail.com>
2023-12-14 06:13:44 +0530

    Shell script

--- 13-install-package.sh
+++ 13-install-package.sh
@@ -10,6 +10,8 @@ then
 echo "User logged in as root user"
 else
 echo "user logged in as normal user"
+exit 1         # this command will exit from the programme.
 fi
 
 yum install nginx -y
+echo "checking the status after exit 1 command"
