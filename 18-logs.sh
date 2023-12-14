db2c26a0655125fc74e6efb852270299adeb33f4
knbob <email2nagababu@gmail.com>
2023-12-14 15:59:29 +0530

    Shell script

--- 18-logs.sh
+++ 18-logs.sh
@@ -5,8 +5,8 @@
 #if command failure &>>logfile -- entries of both success and failure.
 
 ID=$(id -u)
-DATE=$(date +%x_%X)
-LOGFILE="/tmp/$0-$DATE.log"
+TIMESTAMP=$(date +%F:%H:%M:%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 yum install mysql -y &>> $LOGFILE
 
