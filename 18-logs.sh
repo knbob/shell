60702a27b7439c23bcf7c57abd9175744f5b2805
knbob <email2nagababu@gmail.com>
2023-12-14 11:17:27 +0530

    Shell script

---
+++ 18-logs.sh
@@ -0,0 +1,31 @@
+#!/bib/bash
+
+#This program introduces the log entry to a log file.
+#rediction 1>>logfile -- entries if command success 2>>loggile -- entries
+#if command failure &>>logfile -- entries of both success and failure.
+
+ID=$(id -u)
+DATE=$(date +%x-%X)
+LOGFILE=/tmp/$0-$DATE.log
+
+VALIDATE(){
+    if [ $1 == 0 ]
+    then
+    echo " $2 is success"
+    else
+    echo "$2 is failed"
+    fi
+}
+
+if [ $ID == 0 ]
+then
+echo "user logged in as root user"
+else
+echo " Please loggin as root user"
+exit 1
+fi
+
+yum install git -y &>>LOGFILE
+VALIDATE $? "Installing git package"
+yum install nginx -y &>>LOGFILE
+VALIDATE $? "Installing nginx package"
