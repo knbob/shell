095eb42423a7460b6d3ab42939e2dd4964143b90
knbob <email2nagababu@gmail.com>
2023-12-20 18:26:34 +0530

    Optargs

--- 24-for-loop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in /etc/
-do
-    echo "$(file)"
-done
---
+++ 24-opt-args.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+name=""
+wishes=""
+
+USAGE(){
+    echo "Command Usage:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options: "
+    echo " -n, Specify the name (mandatory argument)"
+    echo " -w, Specify the wishes. (optional argument). Default is Good morning"
+    echo " -h, Display Help and exit"
+}
+
+
+while getopts ":n:w:h" opt;
+do
+    case $opt in
+        n) name="$otparg";;
+        w) wishes="$optarg";;
+        h) USAGE; exit;;
+        \?) echo "invalid options: -"$optarg"" >&2; USAGE; exit;;
+    esac
+done
+
+if [ -z "$name"]
+then
+    echo "ERROR: -n is mandatory"
+    USAGE
+    exit 1
+fi
+
+echo "Hello $name. $wishes. I have been learning shell script"
