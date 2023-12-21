83cd32390c7a8a04db3980225c0ae99d153410d6
knbob <email2nagababu@gmail.com>
2023-12-21 14:26:38 +0530

    ec2-instance

--- ec2-instance.sh
+++ ec2-instance.sh
@@ -2,7 +2,8 @@
 
 AMI=ami-03265a0778a880afb
 SG_ID=sg-090563ec8e2cd0ae4
-
+HOSTED_ZONE_ID=Z08178632CFH6K6LABWUB
+DOMAIN_NAME="knbob.online"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "cart" "catalogue" "user" "shipping" "payment" "web")
 
 for i in "${INSTANCES[@]}"
@@ -15,5 +16,20 @@ do
         INSTANCE_TYPE="t2.micro"
     
     fi
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE  --security-group-ids $SG_IDb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE  --security-group-ids $SG_IDb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
+
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
+
 done
